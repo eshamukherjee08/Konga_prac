92177d175f7895784de81e6dd0cd03f1b6874240
EshaMukherjee <vinsol@EshaMukherjee-MacBook-Pro.local>
2012-05-31 13:32:44 +0530

    completed dropdown

--- javascript/konga.js
+++ javascript/konga.js
@@ -5,4 +5,33 @@ $(document).ready(function(){
     var src_num = $(this).find('img').attr("src").match(/[1-3]/);
      $('.img-show').html("<img src = 'images/main_view/main-img"+src_num[0]+".png'>");
 	});
+	
+	$(".dropdown dt a").click(function() {
+	    $(".dropdown dt:eq(0)").toggleClass('backdrop');
+      $(".dropdown dd ul.main").toggle();
+  });
+  
+  $(".dropdown ul.main li#movie a:eq(0), .dropdown ul.main li#gaming a:eq(0), .dropdown ul.main li#books a:eq(0), .dropdown ul.main li#mt a:eq(0)").click(function() {
+      $(this).parent().find('div.list-show').toggle();
+      $(this).parent().addClass('active');
+  });
+  
+  $(document).bind('click', function(e) {
+      var $clicked = $(e.target);
+      if ($clicked.parent().siblings().hasClass("active")){
+        $clicked.parent().siblings().removeClass("active");
+        $clicked.parent().siblings().find('div.list-show').hide();
+      }
+  });
+  
+  $(document).bind('click', function(e) {
+      var $clicked = $(e.target);
+      if (! $clicked.parents().hasClass("dropdown")){
+        $(".dropdown dd ul.main").hide();
+        $(".dropdown ul.main").find("li.active").find('div.list-show').hide();
+        if (! $clicked.parents().hasClass("backdrop"))
+          $(".dropdown dt:eq(0)").removeClass('backdrop');
+          $(".dropdown ul.main").find("li.active").removeClass('active');
+      }
+  });
 });
